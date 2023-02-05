1d88079b86ba6d427750993bff04955bb9758782
prex-aliya <droved-through@proton.me>
2023-02-04 19:40:06 -0600

    For Continued  https://go.dev/tour/flowcontrol/2

--- 001initial/hello.go
+++ 001initial/hello.go
@@ -6,9 +6,9 @@ import (
 
 
 func main() {
-        sum := 0
-        for i := 0; i < 10; i++ {
-                sum += i
+        sum := 1
+        for ; sum < 1000; {
+                sum += sum
         }
         fmt.Println(sum)
 }
