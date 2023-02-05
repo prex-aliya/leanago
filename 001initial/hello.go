56fdbb08418067741ead0be21594e23b72e17573
prex-aliya <droved-through@proton.me>
2023-02-04 19:13:58 -0600

    Variables go.dev/tour/basics/8

--- 001initial/hello.go
+++ 001initial/hello.go
@@ -2,16 +2,9 @@ package main
 
 import "fmt"
 
-func split(sum int) (x, y int) {
-        x = sum * 4 / 9
-        y = sum - x
-
-        /* A return statement without arguments returns 
-         * the named return values.
-         * This is known as a "naked" return */
-        return
-}
+var c, python, java bool
 
 func main() {
-        fmt.Println(split(17))
+        var i int
+        fmt.Println(i, c, python, java)
 }
