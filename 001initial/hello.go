f14370c486c66378e0d04bfef6d0ca8a010661c5
prex-aliya <droved-through@proton.me>
2023-02-04 19:43:17 -0600

    Forever  https://go.dev/tour/flowcontrol/5

--- 001initial/hello.go
+++ 001initial/hello.go
@@ -2,10 +2,17 @@ package main
 
 import (
         "fmt"
+        "math"
 )
 
 
-func main() {
-        for {
+func sqrt(x float64) string {
+        if x < 0 {
+                return sqrt(-x) + "i"
         }
+        return fmt.Sprint(math.Sqrt(x))
+}
+
+func main() {
+        fmt.Println(sqrt(2), sqrt(-4))
 }
