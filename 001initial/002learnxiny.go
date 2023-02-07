4074d6871c6f1e85bb485873b188dedf79c7fff5
prex-aliya <droved-through@proton.me>
2023-02-06 21:17:02 -0600

    🧪Update 002:arrays, slices

--- 001initial/002learnxiny.go
+++ 001initial/002learnxiny.go
@@ -48,3 +48,53 @@ func beyondHello() {
 func learnMultiple(x, y int) (sum, prod int) {
         return x + y, x * y /* Return two values */
 }
+
+/* Built-in types and literals. */
+func learnTypes() {
+        str := "Learn To Program In Go!"
+
+        st := `A "raw" string literal can include line breaks.`
+        /* Same String Type */
+
+        /* Non-ASCII literal The Source UTF-8 */
+        g := 'Σ' /* runt type, an alias for int32, holts unicode */
+
+        f := 7.461262   /* float64, an 64-bit floating point */
+        c := 3 + 4i     /* complex 128, represented internally with two float64's */
+
+        /* Var syntax with initilizers */
+        var u uint = 7
+        var pi float32 = 22. / 7
+
+        /* Conversion syntax with a short declaration */
+        n := byte('\n') /* byte alias for uint8 */
+        
+        /* Arrays have size fixed compile times */
+        var a4 [4]int   /* Array of 4ints, initialized to all 0 */
+        a5 := [...]int{3, 1, 5, 10, 100}
+        /* Array initialized fith a fixed size of five */
+
+        /* Arrays have value semantics */
+        a4_cpy := a4    /* this copy of a4, two separates instances */
+        a4_cpy[0] = 25  /* Only a4_cpy is changed a4 stays the same */
+        fmt.Println(a4_cpy[0] == a4[0])
+
+        /* Slices have dynamic size 
+         * Arrays and slices each have advantages but use cases for slices 
+         * are much more common. */
+         s3 := []int{4, 5, 9}   /* no ellipsis  */
+         s4 := make([]int, 4)   /* slice 4 ints, init to all 0 */
+         var d2 [][]float64     /* Declare only no allocated */
+         bs := []byte("a slice")/* Type conversion */
+
+         /* Slices have reference semantics */
+         s3_cpy := s3   /* Both variables point to the same instance */
+         s3_cpy[0] = 0  /*      both are then updated */
+         fmt.Println(s3_cpy[0] == s3[0])        /* Prints true */
+
+         /* Slices can be appended to on demand
+          * use append() to append elements, first arg is a slice to append*/
+          s := []int{1, 2, 3}   /* Result is a slice of len 3 */
+          s = append(s, 4, 5, 6)        /* add 3 elements. slice now len 6 */
+          fmt.Println(s)        /* Update slice */
+  }
